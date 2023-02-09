dd611d6a07e1fbb8185d0b56eb037ce5bb303d55
Jamilyasbarb <mily.yasbarb@gmail.com>
2023-02-09 17:02:27 -0300

    feat: inputs e api
    adicionado inputs para receber login e senha e mandando para requisicao post para validar se o usuario esta cadastrado

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tarefas_wear/services/usuario_service.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,33 +9,79 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  TextEditingController email = TextEditingController();
+  TextEditingController senha = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SizedBox(
-        width: 200,
+        width: MediaQuery.of(context).size.width,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            Text('Login'),
-            TextFormField(
-              decoration: InputDecoration(
-                labelText: 'Email',
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(30)
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(30)
-                )
+            Expanded(
+              flex: 2,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Image.asset('assets/images/logo.png', width: 20,),
+                  Text('Login'),
+                ],
               ),
             ),
-            TextFormField(
-              decoration: InputDecoration(
-                labelText: 'Senha',
-                border: OutlineInputBorder(),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(30)
-                )
+            Expanded(
+              flex: 3,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    width: 150,
+                    height: 30,
+                    child: TextFormField(
+                      keyboardType: TextInputType.emailAddress,
+                      controller: email,
+                      decoration: InputDecoration(
+                        labelText: 'Email',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30)
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30)
+                        )
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 150,
+                    height: 30,
+                    child: TextFormField(
+                      controller: senha,
+                      decoration: InputDecoration(
+                        labelText: 'Senha',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30)
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30)
+                        )
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: Column(
+                children: [
+                  TextButton(
+                    onPressed: (){
+                      UsuarioService().login(email.text.toString(), senha.text.toString());
+                    }, 
+                    child: Text('Entrar')
+                  ),
+                ],
               ),
             ),
           ],
