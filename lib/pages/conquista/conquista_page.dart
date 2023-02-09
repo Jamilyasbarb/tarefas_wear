30e040d9ec46f480a1463b54a489b5bebbe721a4
Jamilyasbarb <mily.yasbarb@gmail.com>
2023-02-09 17:00:35 -0300

    feat: tela de conquista

---
+++ lib/pages/conquista/conquista_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ConquistaPage extends StatefulWidget {
+  const ConquistaPage({super.key});
+
+  @override
+  State<ConquistaPage> createState() => _ConquistaPageState();
+}
+
+class _ConquistaPageState extends State<ConquistaPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
