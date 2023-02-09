7bdbf57ad9ace29f92374a93cb19c4000970b1ee
Jamilyasbarb <mily.yasbarb@gmail.com>
2023-02-09 17:03:39 -0300

    feat: dados api
    recebendo dados da api e iniciando formatacao

--- lib/pages/tarefa/tarefa_page.dart
+++ lib/pages/tarefa/tarefa_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:tarefas_wear/models/tarefa_model.dart';
+import 'package:tarefas_wear/services/tarefa_service.dart';
 
 class TarefaPage extends StatefulWidget {
   const TarefaPage({super.key});
@@ -8,8 +10,61 @@ class TarefaPage extends StatefulWidget {
 }
 
 class _TarefaPageState extends State<TarefaPage> {
+  List<TarefaModel>? tarefas;
+  bool sucessConnection = false;
+
+  
+  getListaTarefa() async{
+    tarefas = await TarefaService().getTarefas();
+    if(tarefas != null){
+      setState(() {
+        sucessConnection = true;
+      });
+    }
+  }
+  @override
+  void initState() {
+    super.initState();
+    getListaTarefa();
+  }
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: Visibility(
+          visible: sucessConnection,
+          replacement: CircularProgressIndicator(),
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width,
+            child: ListView.builder(
+              itemCount: tarefas?.length,
+              itemBuilder: (context, index) {
+                return Column(
+                  children: [
+                    SizedBox(
+                      width: 150,
+                      height: 100,
+                      child: Card(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text(tarefas![index].titulo),
+                            Text('Correr 8km em 3h'),
+                            TextButton(
+                              onPressed: (){}, 
+                              child: Text('Terminar')
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                );
+              },
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
